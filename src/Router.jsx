5c741e581efccb07acf8fa66a598e072ae857845
tlefrancois <theodore.lefrancois2906@gmail.com>
2021-01-19 10:51:28 +0100

    Prepare the router and make the route

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,7 +1,27 @@
-import "./App.css";
-
-function App() {
-  return <div className="App"></div>;
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import Header from "./Components/Header";
+import Funzone from "./Components/Funzone";
+import Testimonials from "./Components/Testimonials";
+function MyRouter() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/Funzone">
+            <Funzone />
+          </Route>
+          <Route path="/testimonials">
+            <Testimonials />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
-export default App;
+export default MyRouter;
